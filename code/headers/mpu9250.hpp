f422b32c70caa6826054e2c1388dc5a62f9f94d5
SjoerddzHu <31765239+SjoerddzHu@users.noreply.github.com>
2019-07-06 13:59:41 +0200

    added the function declarations for the magnetormeter functions

--- code/headers/mpu9250.hpp
+++ code/headers/mpu9250.hpp
@@ -57,6 +57,95 @@ namespace r2d2::gyroscope {
          */
         accel_data_s get_accel_xyz();
 
+        /**
+         * return the magnetometer measurement in the x direction 
+         */
+        float getMag_X_uT();
+        /**
+         * return the magnetometer measurement in the y direction
+         */
+        float getMag_Y_uT();
+        /**
+         * return the magnetometer measurement in the z direction
+         */
+        float getMag_Z_uT();
+
+        /**
+         * finds bias and scale factor calibration for the magnetometer,
+         * the sensor should be rotated in a figure 8 motion until complete
+         */
+        int calibrate_Mag();
+
+        /**
+         * return the magnetometer bias in the X direction
+         */
+        float get_MagBias_X_uT();
+
+        /**
+         * return the magnetometer scale factor in the X direction
+         */
+        float get_MagScaleFactor_X();
+
+        /**
+         * return the magnetometer bias in the Y direction
+         */
+        float get_MagBias_Y_uT();
+
+        /**
+         * return the magnetometer scale factor in the Y direction
+         */
+        float get_MagScaleFactor_Y();
+
+        /**
+         * return the magnetometer bias in the Z direction
+         */
+        float get_MagBias_Z_uT();
+
+        /**
+         * return the magnetometer scale factor in the Z direction
+         */
+        float get_MagScaleFactor_Z();
+
+        /**
+         * set the magnetometer bias (uT) and scale factor in the X direction
+         */
+        void set_MagCal_X(float bias,float scaleFactor);
+
+        /**
+         * set the magnetometer bias (uT) and scale factor in the Y direction 
+         */
+        void set_MagCal_Y(float bias,float scaleFactor);
+
+        /**
+         * set the magnetometer bias (uT) and scale factor in the Z direction
+         */
+        void set_MagCal_Z(float bias,float scaleFactor);
+
+    protected:
+        float _magScaleX, _magScaleY, _magScaleZ;
+            // magnetometer bias and scale factor estimation
+        uint16_t _maxCounts = 1000;
+        float _deltaThresh = 0.3f;
+        uint8_t _coeff = 8;
+        uint16_t _counter;
+        float _framedelta, _delta;
+        float _hxfilt, _hyfilt, _hzfilt;
+        float _hxmax, _hymax, _hzmax;
+        float _hxmin, _hymin, _hzmin;
+        float _hxb, _hyb, _hzb;
+        float _hxs = 1.0f;
+        float _hys = 1.0f;
+        float _hzs = 1.0f;
+        float _avgs;
+        // transformation matrix
+        /* transform the accel and gyro axes to match the magnetometer axes */
+        const int16_t tX[3] = {0,  1,  0}; 
+        const int16_t tY[3] = {1,  0,  0};
+        const int16_t tZ[3] = {0,  0, -1};
+        // constants
+        const float G = 9.807f;
+        const float _d2r = 3.14159265359f/180.0f;
+
     private:
         /**
          * I2C bus from R2D2
